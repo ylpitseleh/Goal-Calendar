9b293ba52bee8c4fce9ba8251483d4f8ca794256
Hyuk Min <judicious210@gmail.com>
2020-03-27 22:13:25 +0900

    Add Login Validator

--- src/main/java/com/prj/cal/calendar/controller/NoteController.java
+++ src/main/java/com/prj/cal/calendar/controller/NoteController.java
@@ -39,28 +39,27 @@ public class NoteController {
 		return dateFormat.format(date);
 	}
 
-	
-	
 	@RequestMapping(value = "/saveNoteContent", method = RequestMethod.POST)
 	@ResponseBody
 	public void saveNote(Note note, HttpSession session, Member member) {
-		//getAttribute() 는 리턴 타입이 Object이므로 사용시 실제 할당된 객체 타입으로 casting 해야 함.
-		Object tmp_mem = session.getAttribute("member");
-		Member mem = (Member)tmp_mem;
-		note.setNoteId(mem.getMemId()); //현재 세션의 id값을 넣어줌.
-		System.out.println("NoteID(temp) : "+mem.getMemId());
-		
-		service.noteRegister(note);
-		
-		
-		System.out.println("NoteContent in controller : " + note.getNoteContent());
-		System.out.println("NoteId in controller : " + note.getNoteId());
+		// getAttribute() 는 리턴 타입이 Object이므로 사용시 실제 할당된 객체 타입으로 casting 해야 함.
+		// Object tmp_mem = session.getAttribute("member");
+		// Member mem = (Member)tmp_mem;
+		Member mem = (Member) session.getAttribute("member");
+
+		if (mem == null) {
+			System.out.println("Need Login!");
+		} else {
+			note.setNoteId(mem.getMemId()); // 현재 세션의 id값을 넣어줌.
+			System.out.println("NoteID(temp) : " + mem.getMemId());
+
+			service.noteRegister(note);
+
+			System.out.println("NoteContent in controller : " + note.getNoteContent());
+			System.out.println("NoteId in controller : " + note.getNoteId());
+		}
 	}
-	
-	
-	
-	
-	
+
 	@RequestMapping("/test")
 	public String memLogout(Note note) {
 		return "test";
